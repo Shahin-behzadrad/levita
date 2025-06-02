fb33d204c84e61e1c50ea7fe41bb3079fd8198f7
shahin behzadrad <shahinbzr2267@gmail.com>
2025-06-02 17:03:39 +0330

    fix: remove heal-analysis navigations just for now

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,18 +5,19 @@ import { api } from "../../../convex/_generated/api";
 import { useQuery } from "convex/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import LoadingModal from "@/components/LoadingModal/LoadingModal";
 
 export default function ProfilePage() {
   const userData = useQuery(api.userProfiles.getUserProfile);
   const router = useRouter();
 
   useEffect(() => {
-    if (!userData?.role) {
+    if (userData === null) {
       router.push("/");
     }
   }, [userData?.role, router]);
 
-  if (!userData) return null;
+  if (!userData) return <LoadingModal />;
 
   return <ProfileScreen userData={userData} />;
 }
